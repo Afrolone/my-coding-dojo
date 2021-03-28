339b582da45d76ffeb0e6f907e4e852273bae068
WorldWideWest <dzeno.dzafic@windowslive.com>
2021-03-28 12:56:14 +0200

    Cleaning up the code

--- first/first.py
+++ first/first.py
@@ -9,16 +9,3 @@ def findLargest(numbers):
         return largestNumber
     else:
         return None
-
-# doh! python doesn't actually have arrays!
-arr1 = [6, 3, 8, 7, 15, 25, 33, 22, 12]
-arr2 = [9, 2, 1, 5, 15, 65, 73, 12, 52]
-arr3 = [0]
-arr4 = []
-arr5 = [-9, 2, -1, 5, 15, 65, -73, 12, 52]
-
-#print(findLargest(arr1)) # 33
-#print(findLargest(arr2)) # 73
-#print(findLargest(arr3)) # 0
-#print(findLargest(arr4)) # None
-#print(findLargest(arr5)) # 65
